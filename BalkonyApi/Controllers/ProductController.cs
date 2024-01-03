0bcf79c16f0f13e7bb2c02c44773258bdd7bfab4
Diogenes <egeyenigezer1919@gmail.com>
2024-01-03 16:25:29 +0300

    #Projeye Api Eklendi.

---
+++ BalkonyApi/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BalkonyApi.Controllers
+{
+    [ApiController]
+    [Route("[Action]")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
