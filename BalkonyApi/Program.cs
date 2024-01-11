5d35a7c2afb04ae4cc23815d4e25e1d5c808e81e
Diogenes <egeyenigezer1919@gmail.com>
2024-01-11 14:48:11 +0300

    #bilal swaggercı

--- BalkonyApi/Program.cs
+++ BalkonyApi/Program.cs
@@ -37,7 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
